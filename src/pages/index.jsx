ec6dce9219509e8a04251f1b4db18856cb828471
Ayush Singh <asayushsingh80@gmail.com>
2023-07-06 04:45:16 +0530

    added outline to images in homepage

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -12,24 +12,24 @@ export default function Home() {
           <Button text={"Sign Up"} route={"/signup"} />
         </div>
       </div>
-      <div className="hidden relative xl:block flex-grow h-full animate-fade fill-mode-forwards">
-        <div className="h-96 w-96 scale-100 absolute top-[55%] left-[10%] rounded-full overflow-hidden flex justify-center items-center z-10 transition-all hover:scale-125 hover:z-50">
+      <div className="hidden relative xl:block flex-grow h-full animate-fade opacity-0 fill-mode-forwards">
+        <div className="h-96 w-96 scale-100 absolute top-[55%] left-[10%] rounded-full overflow-hidden flex justify-center items-center z-10 transition-all hover:scale-125 hover:z-50 hover:outline outline-4 outline-black">
           <img
             src="/images/person1.jpg"
             alt="person"
             className="scale-125 translate-y-40"
           />
         </div>
-        <div className="h-96 w-96 scale-100 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full overflow-hidden flex justify-center items-center z-40 transition-all hover:scale-125 hover:z-50">
+        <div className="h-96 w-96 scale-100 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full overflow-hidden flex justify-center items-center z-40 transition-all hover:scale-125 hover:z-50 hover:outline outline-4 outline-black">
           <img src="/images/person2.jpg" alt="person" className="scale-150" />
         </div>
-        <div className="h-96 w-96 scale-100 absolute rounded-full top-[25%] right-[10%] overflow-hidden transition-all hover:scale-125 hover:z-50">
+        <div className="h-96 w-96 scale-100 absolute rounded-full top-[25%] right-[10%] overflow-hidden transition-all hover:scale-125 hover:z-50 hover:outline outline-4 outline-black">
           <img src="/images/person3.jpg" alt="person" className="" />
         </div>
-        <div className="h-96 w-96 scale-100 absolute rounded-full top-[25%] left-[10%] overflow-hidden flex justify-center items-center transition-all hover:scale-125 hover:z-50">
+        <div className="h-96 w-96 scale-100 absolute rounded-full top-[25%] left-[10%] overflow-hidden flex justify-center items-center transition-all hover:scale-125 hover:z-50 hover:outline outline-4 outline-black">
           <img src="/images/person4.jpg" alt="person" className="" />
         </div>
-        <div className="h-96 w-96 scale-100 absolute rounded-full top-[55%] right-[10%] overflow-hidden flex justify-center items-center transition-all hover:scale-125 hover:z-50">
+        <div className="h-96 w-96 scale-100 absolute rounded-full top-[55%] right-[10%] overflow-hidden flex justify-center items-center transition-all hover:scale-125 hover:z-50 hover:outline outline-4 outline-black">
           <img src="/images/person5.jpg" alt="person" className="" />
         </div>
       </div>
