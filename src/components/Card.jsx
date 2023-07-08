ed6bcf223b21d5e34cdd4e1d4ff23925e4c5dec5
Ayush Singh <asayushsingh80@gmail.com>
2023-07-08 20:08:09 +0530

    added dynamic component for blogs

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 export default function Card({ title, author }) {
   return (
-    <div className="bg-white p-5 flex flex-col gap-4 rounded-xl">
+    <div className="bg-white p-5 flex flex-col gap-4 rounded-xl transition-all hover:bg-color-primary-light active:bg-color-primary cursor-pointer">
       <span className="text-4xl">{title}</span>
       <span className="text-xl self-end">{author}</span>
     </div>
