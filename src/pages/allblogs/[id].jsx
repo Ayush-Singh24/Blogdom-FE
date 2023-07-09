bc64a9a8dd6ea8c988f03a82f1f8d7ad3ea8d150
Ayush Singh <asayushsingh80@gmail.com>
2023-07-09 19:14:42 +0530

    added addblog page

--- src/pages/allblogs/[id].jsx
+++ src/pages/allblogs/[id].jsx
@@ -3,6 +3,7 @@ import { useAuth } from "@/hooks/useAuth";
 import { openAlert } from "@/redux/reducers/alert";
 import { Service } from "@/service/service";
 import { AlertStatus, ResponseStatus } from "@/utils/constants";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
@@ -38,20 +39,29 @@ export default function Details() {
   }
 
   return (
-    <section className="m-5 bg-color-card-container rounded-3xl">
-      <div className="p-5 bg">
-        <button
-          className="bg-white rounded-full p-2"
-          onClick={() => router.push("/allblogs")}
-        >
-          <img src="/icons/arrow-left.svg" alt="back" />
-        </button>
+    <>
+      <section className="m-5 bg-color-card-container rounded-3xl">
+        <div className="p-5 bg">
+          <button
+            className="bg-white rounded-full p-2"
+            onClick={() => router.push("/allblogs")}
+          >
+            <img src="/icons/arrow-left.svg" alt="back" />
+          </button>
+        </div>
+        <div className="flex flex-col mb-4 p-5 bg-slate-300 shadow-black shadow-md rounded-t-3xl rounded-b-3xl transition-all hover:bg-color-primary hover:text-white">
+          <h1 className="text-5xl">{blogData.title}</h1>
+          <span className="self-end text-2xl">{blogData.authorName}</span>
+        </div>
+        <p className="p-5 text-3xl">{blogData.blogContent}</p>
+      </section>
+      <div className="flex justify-center ">
+        <Link href="/allblogs" className="hover:scale-105">
+          <span className="text-white py-4 text-4xl underline underline-offset-4 cursor-pointer  transition-all">
+            Go back
+          </span>
+        </Link>
       </div>
-      <div className="flex flex-col mb-4 p-5 bg-slate-300 shadow-black shadow-md rounded-t-3xl rounded-b-3xl transition-all hover:bg-color-primary hover:text-white">
-        <h1 className="text-5xl">{blogData.title}</h1>
-        <span className="self-end text-2xl">{blogData.authorName}</span>
-      </div>
-      <p className="p-5 text-3xl">{blogData.blogContent}</p>
-    </section>
+    </>
   );
 }
