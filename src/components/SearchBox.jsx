c8bbe52fddad84eedb39376ee52a9c2219ba8cdc
Ayush Singh <asayushsingh80@gmail.com>
2023-07-13 02:02:46 +0530

    Added logo and neccessary media queries for it

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -2,16 +2,21 @@ import Link from "next/link";
 
 export default function SearchBox({ searchInput, setSearchInput }) {
   return (
-    <div className="flex gap-5 bg-color-card-container justify-center items-center py-2 px-4">
+    <div className="flex gap-5 bg-color-card-container justify-center items-center py-2 px-4 lg:justify-between">
+      <img
+        src="/images/blogdom_icon.png"
+        alt="logo"
+        className="h-20 hidden sm:block"
+      />
       <input
         type="text"
         placeholder="Search"
         name="blog-search"
         value={searchInput}
         onChange={(event) => setSearchInput(event.target.value.trimStart())}
-        className="rounded-full p-4 w-[80%] text-2xl lg:w-1/2 lg:mr-auto"
+        className="rounded-full p-4 w-[80%] text-2xl lg:w-1/2 sm:mx-auto"
       />
-      <nav className="hidden lg:flex lg:gap-10 lg:p-5 lg:text-2xl lg:font-semibold lg:justify-end text-color-secondary">
+      <nav className="hidden lg:flex lg:gap-10 lg:p-5 lg:text-2xl lg:font-semibold text-color-secondary">
         <Link
           href="/allblogs"
           className="relative before:content-[''] before:h-1  before:absolute before:top-full before:left-0 before:w-0 before:transition-all before:bg-color-primary-light hover:before:w-full"
